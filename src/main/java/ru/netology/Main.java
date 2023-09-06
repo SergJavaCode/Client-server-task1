171405c3fe86b57c39369b2a307b7ac0ba42db07
Serg <freekey@list.ru>
2023-09-06 23:51:41 +0400

    add new functional

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -10,22 +10,60 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("старт сервера");
-        int port=8090;
-        try (ServerSocket serverSocket = new ServerSocket(port); ) {// порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
-            while (true){
+        int port = 8090;
+        try (ServerSocket serverSocket = new ServerSocket(port);) {// порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+            while (true) {
                 try (Socket clientSocket = serverSocket.accept(); // ждем подключения
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));)
-                {
-                    System.out.println("New connection accepted");
-                    final String name = in.readLine();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
 
-                    out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                    System.out.println("New connection accepted");
+                    out.println("Назовите своё имя:");
+                    String entry = in.readLine();
+                    final String name = entry;
+                    System.out.println("Сервер принял имя: " + name);
+                    out.println("Ведите свой возраст:");
+                    int age = Integer.parseInt(in.readLine());
+                    System.out.println("Сервер принял возраст: " + age);
+                    out.println(String.format("Привет %s, твой возраст %d", name, age));
+                    if (age >= 18) {
+                        out.println("Вы можете претендовать на работу в нашей организации!");
+                        out.println("Вы готовы приступить к работе? (yes/no)");
+                        String result = in.readLine();
+                        if (result.equalsIgnoreCase("yes")) {
+                            out.println("Вы приняты на работу!");
+                        } else {
+                            out.println("Подумайте еще. Мы свяжемся с вами позже");
+                        }
+
+                    } else {
+                        out.println("Вы можете пройти обучение в нашей организации!");
+                        out.println("Вы готовы приступить к обучению? (yes/no)");
+                        String result = in.readLine();
+                        if (result.equalsIgnoreCase("yes")) {
+                            out.println("Вы зачислены на обучение!");
+                        } else {
+                            out.println("Подумайте еще. Мы свяжемся с вами позже");
+                        }
+
+                    }
+
+                    // выключаем соединения
+                    System.out.println("Client disconnected");
+                    System.out.println("Closing connections & channels.");
+                    // закрываем сначала каналы сокета !
+                    in.close();
+                    out.close();
+                    // потом закрываем сам сокет общения на стороне сервера!
+                    serverSocket.close();
+                    // потом закрываем сокет сервера который создаёт сокеты общения
+                    // хотя при многопоточном применении его закрывать не нужно
+                    // для возможности поставить этот серверный сокет обратно в ожидание нового подключения
+                    System.out.println("Closing connections & channels - DONE.");
                 }
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e);
         }
-
     }
 }
--- src/main/java/ru/netology/client/Client.java
+++ src/main/java/ru/netology/client/Client.java
@@ -5,19 +5,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        String host = "127.0.0.1";
+        String host = "netology.homework";
         int port = 8090;
         try (Socket clientSocket = new Socket(host, port);
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in =new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-
-        ){
-            out.println("Netology");
-            String resp= in.readLine();
-            System.out.println(resp);
-        } catch (IOException e) {e.printStackTrace();}
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
+            String serverWord = in.readLine();
+            System.out.println(serverWord);
+            String message = br.readLine();// получаем ввод
+            out.println(message);//отправляем на сервер
+            serverWord = in.readLine(); // ждём, что скажет сервер
+            System.out.println(serverWord); // получив - выводим на экран
+            message = br.readLine();// получаем ввод
+            out.println(message);//отправляем на сервер
+            serverWord = in.readLine(); // ждём, что скажет сервер
+            System.out.println(serverWord); // получив - выводим на экран
+            serverWord = in.readLine(); // ждём, что скажет сервер
+            System.out.println(serverWord); // получив - выводим на экран
+            serverWord = in.readLine(); // ждём, что скажет сервер
+            System.out.println(serverWord); // получив - выводим на экран
+            message = br.readLine();    // получаем ввод
+            out.println(message);       //отправляем на сервер
+            serverWord = in.readLine(); // ждём, что скажет сервер
+            System.out.println(serverWord); // получив - выводим на экран
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
